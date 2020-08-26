950a46eac4cd500697cf6dcc019a14878f7e3608
mathandotcom <mathana.marimuthu@gmail.com>
2020-08-26 18:18:20 -0400

    New

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello from Node Server App deployed on ECS!! Congratulations - v4');
+  response.end('Hello from Node Server App deployed on ECS!! Congratulations - Anitha Success');
   
 };
 var www = http.createServer(handleRequest);
