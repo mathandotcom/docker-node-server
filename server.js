4241f49603e193c703bc20ae2fb3b693a952ec15
mathandotcom <40619437+mathandotcom@users.noreply.github.com>
2020-09-01 16:50:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello from Node Server App deployed on ECS!! Congratulations -Bala');
+  response.end('Hello from Node Server App deployed on ECS!! Congratulations -Bala & Mathan ');
   
 };
 var www = http.createServer(handleRequest);
